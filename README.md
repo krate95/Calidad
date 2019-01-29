b88cd93ef5503ddc75dc2734a0022f7cbc274e1e
krate95 <jesus200495@gmail.com>
2019-01-29 17:32:15 +0100

    Cambios en readme y comentarios

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 
 ![Pantalla de inicio](https://github.com/krate95/Calidad/blob/master/Screenshots/Inicio.jpg)
 ![Pantalla de juego](https://github.com/krate95/Calidad/blob/master/Screenshots/Juego.jpg)
+
+
+## Errores a corregir
+
+Según la resolución del dispositivo donde se ejecuta, los disparos puede que no se vean (en Nexus 5X no se ven y en Pixel 2 XL sí).
+Los botones de control de movimiento cambian de posición según la resolución del dispositivo.
+Si se intenta iniciar el juego con alguno de los dos campos vacío la aplicación sufre un cierre forzoso.
--- SpaceInvaders/app/src/main/java/com/example/aleja/spaceinvaders/SpaceInvaders.java
+++ SpaceInvaders/app/src/main/java/com/example/aleja/spaceinvaders/SpaceInvaders.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.view.Display;
 
 // SpaceInvadersActivity es el punto de entrada al juego.
-// Se va a encargar del ciclo de vida del juego al llamar alos métodos de vistaSpaceInvaders cuando sean solicitados por el SO.
+// Se va a encargar del ciclo de vida del juego al llamar a los métodos de vistaSpaceInvaders cuando sean solicitados por el SO.
 public class SpaceInvaders extends Activity {
 
     // Visualización del juego
@@ -18,7 +18,7 @@ public class SpaceInvaders extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Obtener un objeto de Display para accesar a los detalles de la pantalla
+        // Obtener un objeto de Display para acceder a los detalles de la pantalla
         Display display = getWindowManager().getDefaultDisplay();
         // Cargar la resolución a un objeto de Point
         Point size = new Point();
