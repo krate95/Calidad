25625b3d8a3757cb9204c3e3d2a6c6ad803554ad
krate95 <jesus200495@gmail.com>
2019-02-11 13:20:09 +0100

    Test de ejemplo

---
+++ SpaceInvaders/app/src/test/java/com/example/aleja/spaceinvaders/TestEjemplo.java
@@ -0,0 +1,12 @@
+package com.example.aleja.spaceinvaders;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestEjemplo {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals("Prueba test sencillo",4, 1 + 2);
+    }
+}
