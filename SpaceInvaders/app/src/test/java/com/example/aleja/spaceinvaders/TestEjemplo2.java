afe2ee165415f2501000af6368fac05d6abb38fb
analpa1997 <32297480+analpa1997@users.noreply.github.com>
2019-02-11 14:04:39 +0100

    Test de ejemplo, prueba de Travis

---
+++ SpaceInvaders/app/src/test/java/com/example/aleja/spaceinvaders/TestEjemplo2.java
@@ -0,0 +1,14 @@
+package com.example.aleja.spaceinvaders;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestEjemplo2 {
+
+    @Test
+    public void addition_isCorrect() {
+        assertEquals("Prueba test sencillo ABC",5, 3 + 2);
+    }
+
+}
