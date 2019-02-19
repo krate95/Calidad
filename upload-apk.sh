acb22395077bfb8799352c885cc99013beb32be5
krate95 <jesus200495@gmail.com>
2019-02-19 19:48:36 +0100

    Update upload-apk.sh

--- upload-apk.sh
+++ upload-apk.sh
@@ -1,16 +1,16 @@
  #create the new directory will contain que October generated apk
-mkdir $ HOME/buildApk/ 
+mkdir $HOME/buildApk/ 
 #copy generated apk from build folder to the folder just created
 cp -R app/build/outputs/apk/app-debug.apk $HOME/android/ 
 #go to home and git setup  
-cd $ HOME 
+cd $HOME 
 git config --global user.email "useremail@domain.com" 
 git config --global user.name "Your Name" 
 # Clone the repository in the folder buildApk
 git clone --quiet --branch=master https://krate95:$GITHUB_API_KEY@github.com/krate95/Calidad master > /dev/null 
 #go into directory and copy data we're interested
 cd master 
-cp -Rf $ HOME/android/*. 
+cp -Rf $HOME/android/*. 
 #add, commit and push files
 git add -f .
 git remote add origin https://krate95:$GITHUB_API_KEY@github.com/krate95/Calidad.git
