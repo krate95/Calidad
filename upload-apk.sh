8caa294fa209a11cf8fb50846d6be3c38b3badda
krate95 <jesus200495@gmail.com>
2019-02-19 19:20:01 +0100

    Corregido script de subida

--- upload-apk.sh
+++ upload-apk.sh
@@ -1,19 +1,19 @@
  #create the new directory will contain que October generated apk
-  mkdir $ HOME / buildApk / 
+  mkdir $ HOME/buildApk/ 
   #copy generated apk from build folder to the folder just created
-  cp -R app / build / outputs / apk / app-debug.apk $ HOME / android / 
+  cp -R app/build/outputs/apk/app-debug.apk $ HOME/android/ 
   #go to home and git setup  
   cd $ HOME 
   git config --global user.email "useremail@domain.com" 
   git config --global user.name "Your Name" 
   # Clone the repository in the folder buildApk
-  git clone --quiet --branch master = https: // user-name: $GITHUB_API_KEY@github.com/user-name/repo-name master> / dev / null 
+  git clone --quiet --branch master = https://krate95:$GITHUB_API_KEY@github.com/krate95/Calidad master> /dev/null 
   #go into directory and copy data we're interested
-  cd master cp -Rf $ HOME / android / *. 
+  cd master cp -Rf $ HOME/android/*. 
   #add, commit and push files
-  git add -f.
-  git remote add origin https: // user-name: $GITHUB_API_KEY@github.com/user-name/repo-name.git
-   . git add -f 
+  git add -f .
+  git remote add origin https://krate95:$GITHUB_API_KEY@github.com/krate95/Calidad.git
+  git add -f 
   git commit -m "Travis build $ TRAVIS_BUILD_NUMBER pushed [skip ci] " 
   git push origin master -fq> / dev / null 
-  echo -e" Done \ n "
+  echo -e "Done\n"
