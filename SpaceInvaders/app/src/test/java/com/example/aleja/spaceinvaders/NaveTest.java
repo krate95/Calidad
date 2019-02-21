aa1c3d4686a9920b8091c1ef5f6a72e08ec999e6
krate95 <jesus200495@gmail.com>
2019-02-21 17:50:04 +0100

    Test movimiento nave

---
+++ SpaceInvaders/app/src/test/java/com/example/aleja/spaceinvaders/NaveTest.java
@@ -0,0 +1,32 @@
+package com.example.aleja.spaceinvaders;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+public class NaveTest {
+    private SpaceInvaders SpaceInvaders = mock(SpaceInvaders.class);
+    private Nave nave = new Nave(SpaceInvaders, 100, 100);
+
+    @Test
+    public void update() {
+        assertEquals(50, nave.getX(), 1);
+        float Xtemp = nave.getX();
+
+
+        nave.setMovementState(nave.RIGHT);
+        nave.update(30, false, false, false, false);
+        assertEquals(Xtemp+11.6, nave.getX(), 1);
+        Xtemp += 11.6;
+
+
+        nave.update(30, false, false, false, false);
+        assertEquals(Xtemp+11.6, nave.getX(), 2);
+        Xtemp += 11.6;
+
+
+        nave.update(30, true, false, false, false);
+        assertEquals(Xtemp, nave.getX(), 1);
+    }
+}
